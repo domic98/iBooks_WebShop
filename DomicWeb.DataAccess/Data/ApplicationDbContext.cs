f2856426f418b4a646a3943c3d329e63ca67ddb8
domic98 <domagoj.petrec@gmail.com>
2023-12-04 01:00:45 +0100

    Add project files.

---
+++ DomicWeb.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DomicWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomicWeb.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //uvijek se to mora dodat, sve konfiguracije koje dodamo želimo proslijediti te opcije bazi(baza je DbContext)
+        //nakon toga želimo registrirati naš DbContext, to radimo u Program.cs
+        //tu radimo sve vezano za našu bazu podataka
+        //kada želimo dodati tablice moramo dodati DbSet
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } //prvo je klasa<> koju tablicu želimo dodati, drugo naziv tablice
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //već postoji u DbContext zato mora ić override
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action", DisplayOrder=1 },
+                 new Category { Id=2, Name="SciFi", DisplayOrder=2 },
+                  new Category { Id=3, Name="History", DisplayOrder=3 }
+                ); //koristimo za seedanje podataka u tablicu Category
+        }
+    }
+}
---
+++ DomicWeb/Controllers/CategoryController.cs
@@ -0,0 +1,107 @@
+﻿using DomicWeb.DataAccess.Data;
+using DomicWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DomicWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db; //to sve možemo jer smo prvo registrirali u Services u Program.cs
+        public CategoryController(ApplicationDbContext db) //kako da vidimo u index view naše podatke
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList(); //radimo listu za prikaz kategorija
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() //ako ne dodamo HttpGet, po defaulte je Get
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order cannot exactly match the Name"); //key se odnosi na šta govorimo pogreškiu, pod unos Name
+            }
+
+            if (ModelState.IsValid) //ode u model Category i provjeri jesu li svi uvjeti zadovoljeni(npr.not null, dužina imena, itd)
+                                    //ako jesu onda dodaje objekt u Categories i sprema promjene u db
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"]= "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();//ako želimo preusmjerit na drugu akciju koja nije u ovom kontroleru
+                          //onda moramo napisati i u koji kontroler da ode: return RedirectToAction("Index","Category")
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //3 opcije kako naći jednu od kategorija po id-u
+            //Category ?categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"]= "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); 
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"]= "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
