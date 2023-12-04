2b00d1690f00d861f67dbb7837a97afe52f053a6
domic98 <domagoj.petrec@gmail.com>
2023-12-04 16:00:46 +0100

    Adding interface IRepository,ICategoryRepository

---
+++ DomicWeb.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using DomicWeb.DataAccess.Data;
+using DomicWeb.DataAccess.Repository.IRepository;
+using DomicWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomicWeb.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
