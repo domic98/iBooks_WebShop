06a4bc9385a8a55bc05dcd2a33f879f04e6fa8d8
domic98 <domagoj.petrec@gmail.com>
2023-12-08 21:49:58 +0100

    Adding particles.js and Details view.

--- DomicWeb/Areas/Admin/Controllers/ProductController.cs
+++ DomicWeb/Areas/Admin/Controllers/ProductController.cs
@@ -137,6 +137,7 @@ namespace DomicWeb.Areas.Admin.Controllers
             return View(productFromDb);
         }
         */
+        /*
         [HttpPost]
         public IActionResult Edit(Product obj)
         {
@@ -179,14 +180,42 @@ namespace DomicWeb.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
         }
+        */
 
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll(int id)
+        public IActionResult GetAll()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json (new {data = objProductList });
+
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if ( productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting! " });
+            }
+            
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                               productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
 
         }
 
--- DomicWeb/Areas/Customer/Controllers/HomeController.cs
+++ DomicWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using DomicWeb.DataAccess.Repository.IRepository;
 using DomicWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,16 +8,26 @@ namespace DomicWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
