a88fd43c662e92cd896068e804e6c2fe1d91400f
domic98 <domagoj.petrec@gmail.com>
2023-12-13 00:32:07 +0100

    Update Login and Register UI, Adding All Fields

--- DomicWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DomicWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -23,6 +23,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
+
 namespace DomicWeb.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
@@ -110,6 +111,15 @@ namespace DomicWeb.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -146,6 +156,14 @@ namespace DomicWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
