2ccca6adc160c72a0ee61a81160fa911439f502d
domic98 <domagoj.petrec@gmail.com>
2023-12-14 23:40:55 +0100

    Adding and Modify ShoppingCart Button

--- DomicWeb/Areas/Customer/Controllers/HomeController.cs
+++ DomicWeb/Areas/Customer/Controllers/HomeController.cs
@@ -26,9 +26,17 @@ namespace DomicWeb.Areas.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+
+                return View(cart);
         }
+
 
         public IActionResult Privacy()
         {
@@ -40,5 +48,4 @@ namespace DomicWeb.Areas.Customer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
-}
+ }  }
