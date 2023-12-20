a16f25479cb1641e53d3527f63a7c9dbfd4321e7
domic98 <domagoj.petrec@gmail.com>
2023-12-20 12:57:57 +0100

    Order total in Shopping Cart, Dynamic Shopping Cart, Shopping Cart and Summary UI.

--- DomicWeb.DataAccess/Repository/IRepository/IRepository.cs
+++ DomicWeb.DataAccess/Repository/IRepository/IRepository.cs
@@ -11,7 +11,7 @@ namespace DomicWeb.DataAccess.Repository.IRepository
     {
        // T - Category 
         
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, string? includeProperties = null);
         T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
--- DomicWeb.Models/ShoppingCart.cs
+++ DomicWeb.Models/ShoppingCart.cs
@@ -25,5 +25,8 @@ namespace DomicWeb.Models
         [ValidateNever]
 
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
