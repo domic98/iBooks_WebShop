f57b8bd113006d01135a68df61b071b1dd64c453
domic98 <domagoj.petrec@gmail.com>
2024-02-02 16:57:42 +0100

    Adding UserController and  user.js

--- DomicWeb/Areas/Admin/Controllers/UserController.cs
+++ DomicWeb/Areas/Admin/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using DomicWeb.Models;
 using DomicWeb.Models.ViewModels;
 using DomicWeb.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,10 +16,12 @@ namespace DomicWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;  
 
-        public UserController(ApplicationDbContext db)
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
             
 
         }
@@ -27,7 +30,61 @@ namespace DomicWeb.Areas.Admin.Controllers
             return View();
         }
 
-       
+        public IActionResult RoleManagament(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include( u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name; 
+            
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagament(RoleManagementVM roleManagementVM)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if(!(roleManagementVM.ApplicationUser.Role == oldRole))
+            {
+                // a role was updated
+
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+                if(roleManagementVM.ApplicationUser.Role == SD.Role_Company) {
+
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
         #region API CALLS
 
         [HttpGet]
@@ -35,18 +92,53 @@ namespace DomicWeb.Areas.Admin.Controllers
         {
             List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
 
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
 
+            foreach(var user in objUserList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null )
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+
+                }
+            }
+            
 
             return Json (new {data = objUserList });
 
         }
 
 
-        [HttpDelete]
-        public IActionResult Delete(int? id)
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
         {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if ( objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
 
-            return Json(new { success = true, message = "Delete Successful" });
+            if(objFromDb.LockoutEnd != null && objFromDb.LockoutEnd> DateTime.Now)
+            {
+                // user is currently locked and we need to unlock them
+
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Operation Successful" });
 
         }
 
---
+++ DomicWeb/wwwroot/js/roletwo.js
@@ -0,0 +1,25 @@
+﻿var companyData;
+
+$(document).ready(function () {
+    companyView();
+});
+
+function companyView() {
+    companyData =
+        $('#ApplicationUser_Role').change(function () {
+            var selection = $('#ApplicationUser_Role Option:Selected').text();
+            if (selection == 'Company') {
+                $('#ApplicationUser_CompanyId').show();
+            }
+            else {
+                $('#ApplicationUser_CompanyId').hide();
+            }
+        });
+
+
+}
+
+
+
+
+
